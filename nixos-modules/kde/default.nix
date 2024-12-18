4c57cc70afd72a9260124a245374073a206f3131
carp <carp@karpfen.dev>
2024-12-18 23:18:19 +0100

    wip

--- nixos-modules/kde/default.nix
+++ nixos-modules/kde/default.nix
@@ -4,12 +4,6 @@
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    konsole
-    elisa
-  ];
-
   qt = {
     enable = true;
     platformTheme = "gnome";
