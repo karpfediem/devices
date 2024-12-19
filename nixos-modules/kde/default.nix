5fec9e6dd30e6d9e7c4f10b175c0a0756cd08b50
carp <carp@karpfen.dev>
2024-12-19 15:55:23 +0100

    fix: remove QT since it introduces conflicting version

--- nixos-modules/kde/default.nix
+++ nixos-modules/kde/default.nix
@@ -3,10 +3,4 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  qt = {
-    enable = true;
-    platformTheme = "gnome";
-    style = "adwaita-dark";
-  };
 }
