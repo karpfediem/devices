4f23e80cf9d9ae4615978b82d14cfac204fcc637
carp <carp@karpfen.dev>
2024-12-21 13:36:25 +0100

    fix: disable channels as we are using a flake setup

--- nixos-configurations/common.nix
+++ nixos-configurations/common.nix
@@ -3,6 +3,7 @@
     inputs.disko.nixosModules.disko
   ];
 
+  config.nix.channel.enable = false;
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "steam"
     "steam-unwrapped"
