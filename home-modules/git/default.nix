4a08945629cff3f581435b8aa3bc5662f8d358d1
Karpfen <karpfen@karpfen.dev>
2024-12-25 14:23:27 +0100

    feat: add more git aliases

--- home-modules/git/default.nix
+++ home-modules/git/default.nix
@@ -3,5 +3,19 @@
     enable = true;
     userName = "Karpfen";
     userEmail = "karpfen@karpfen.dev";
+    aliases = {
+      ds = "diff --staged";
+      l = "log --color --graph --pretty=slog";
+      lg = "log --color --graph --pretty=lg --abbrev-commit";
+      am = "commit --amend --no-edit";
+      wdiff = "diff -w --word-diff=color --ignore-space-at-eol";
+      as = "add -u";
+    };
+    extraConfig = ''
+    [pretty]
+    lg = format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset
+    slog = format:%C(yellow)%h %Cred%as %Cblue%an%Cgreen%d %Creset%s
+    bw = format:%h | %as | %>(20,trunc)%d%x09%s
+    '';
   };
 }
