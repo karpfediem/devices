4002eacf39f864363ab754610a956665cef11a1a
Karpfen <karpfen@karpfen.dev>
2025-05-13 17:08:04 +0200

    feat(nixos): Set additional binary caches

--- nixos-configurations/common.nix
+++ nixos-configurations/common.nix
@@ -16,4 +16,25 @@
   ];
   nix.channel.enable = false;
   nix.registry.unstable.flake = inputs.nixpkgs-unstable;
+
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
+      "https://devenv.cachix.org"
+      "https://cache.karpfen.dev/sys"
+    ];
+    trusted-substituters = [
+      "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
+      "https://devenv.cachix.org"
+      "https://cache.karpfen.dev/sys"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "sys:L5zpT+DS98U14F+agHzrWzc2KGDrCFdtpkR90YsQqt0="
+    ];
+  };
 }
